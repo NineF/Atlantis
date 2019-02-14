4fca35cee0987450939311d7e35b890d003154b6
ganlun <ganlun@vdiscovery.cn>
2019-02-14 17:52:26 +0800

    动态规划完结

--- src/main/java/com/bulingbuu/base/DPTraining.java
+++ src/main/java/com/bulingbuu/base/DPTraining.java
@@ -32,10 +32,40 @@ public class DPTraining {
         System.out.println(item[money]);
     }
 
+    int[] datas = { 2, 9, 3, 8, 7, 6, 18 };
+
+    /**
+     * 查找最大递增子串
+     *
+     * @param datas
+     */
+    public void increment(int[] datas) {
+        int[] item = new int[datas.length];
+        for (int i = 0; i < datas.length; i++) {
+            item[i] = 1;
+        }
+
+        for (int i = 1; i < datas.length; i++) {
+            int k = i - 1;
+            int tmp = 1;
+            while (k >= 0) {
+                if (datas[i] > datas[k]) {
+                    tmp = Math.max(item[k] + 1, tmp);
+                    break;
+                }
+                tmp = Math.max(item[k], tmp);
+                k--;
+            }
+            item[i] = tmp;
+        }
+
+        System.out.println(item[datas.length - 1]);
+    }
+
     public static void main(String[] args) {
         DPTraining training = new DPTraining();
-        training.change(25);
-
+//        training.change(25);
+        training.increment(training.datas);
     }
 
 }
