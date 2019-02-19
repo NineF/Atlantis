b9e3f19adecf0ddbd6fc7fc3b7917c49c431c415
NineF <894906165@qq.com>
2019-02-20 00:15:05 +0800

    i

--- src/main/java/com/bulingbuu/problem/array/MajorityElement.java
+++ src/main/java/com/bulingbuu/problem/array/MajorityElement.java
@@ -1,5 +1,8 @@
 package com.bulingbuu.problem.array;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author bulingbuu
  * @date 19-2-19 下午6:27
@@ -20,8 +23,92 @@ package com.bulingbuu.problem.array;
  */
 public class MajorityElement {
 
-    public int majorityElement(int[] nums) {
+    public static void main(String[] args) {
+        MajorityElement majorityElement = new MajorityElement();
+        int[] ints = {10, 9, 9, 9, 10};
+        System.out.println(majorityElement.majorityElement(ints));
+        int[] nums = {3, 2, 3};
+        for (int i : majorityElement.majorityElement2(nums)) {
+            System.out.println(i);
+        }
+    }
 
-        return -1;
+    /**
+     * 摩尔投票法
+     *
+     * @param nums
+     * @return
+     */
+    public int majorityElement(int[] nums) {
+        int ret = nums[0];
+        int count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (ret == nums[i]) {
+                count++;
+            } else {
+                count--;
+                if (count == 0) {
+                    i++;
+                    ret = nums[i];
+                    count = 1;
+                }
+            }
+        }
+        return ret;
+    }
+
+    /**
+     * 给定一个大小为 n 的数组，找出其中所有出现超过 ⌊ n/3 ⌋ 次的元素。
+     * <p>
+     * 说明: 要求算法的时间复杂度为 O(n)，空间复杂度为 O(1)。
+     * 示例 1:
+     * <p>
+     * 输入: [3,2,3]
+     * 输出: [3]
+     * 示例 2:
+     * <p>
+     * 输入: [1,1,1,3,3,2,2,2]
+     * 输出: [1,2]
+     *
+     * @param nums
+     * @return
+     */
+    public List<Integer> majorityElement2(int[] nums) {
+        int ret1 = Integer.MAX_VALUE;
+        int ret2 = Integer.MAX_VALUE;
+        int count1 = 0;
+        int count2 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (ret1 == nums[i]) {
+                count1++;
+            } else if (ret2 == nums[i]) {
+                count2++;
+            } else if (count1 == 0) {
+                ret1 = nums[i];
+                count1 = 1;
+            } else if (count2 == 0) {
+                ret2 = nums[i];
+                count2 = 1;
+            }else {
+                count1--;
+                count2--;
+            }
+        }
+        List<Integer> result = new ArrayList<>(2);
+        count1=0;
+        count2=0;
+        for (int i:nums){
+            if (ret1==i){
+                count1++;
+            }else if (ret2==i){
+                count2++;
+            }
+        }
+        if (count1>nums.length/3)
+            result.add(ret1);
+        if (count2>nums.length/3)
+            result.add(ret2);
+
+        return result;
     }
 }
